4ddb0aef36d6c5cb3c65124530659206a5563e21
Nikita <Nikita@DESKTOP-UICP4TN>
2021-11-18 20:32:13 +0500

    Add project files.

---
+++ Configuration.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace Tms.Identity
+{
+    public class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("TmsWebAPI", "Web API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("TmsWebAPI", "Web API", new[]{JwtClaimTypes.Name})
+                {
+                    Scopes = { "TmsWebAPI" }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "notes-web-api",
+                    ClientName = "Notes Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+                        "http://.../signin-oidc"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        "http://..."
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http://.../signout-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "TmsWebAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+    }
+}
---
+++ Data/AppUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tms.Identity.Models;
+
+namespace Tms.Identity.Data
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
